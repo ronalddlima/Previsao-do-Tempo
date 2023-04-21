de7018d85d3ca5833e5dfbaee4b98e3645df09d1
ronalddlima <ronaldjose131@gmail.com>
2023-04-21 12:56:30 -0300

    Inicial

---
+++ scripts.js
@@ -0,0 +1,27 @@
+
+
+
+const key = "5d0ff3f1a6a2327f072999a6a7065b2b"
+
+function colocarDadosNaTela(dados){
+console.log(dados)
+document.querySelector(".cidade").innerHTML = 'Tempo em ' + dados.name
+document.querySelector(".tempo").innerHTML = Math.floor (dados.main.temp) + '°C'
+document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+document.querySelector(".umidade").innerHTML = 'Umidade ' + dados.main.humidity + '%'
+document.querySelector(".img-previsao").src=`https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+}
+
+ async function buscarCidade(cidade){
+
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
+
+    colocarDadosNaTela(dados)
+    
+}
+
+function cliqueiNoBotao(){
+    const cidade = document.querySelector(".input-cidade").value 
+    buscarCidade(cidade)
+}
+
